4cc62d4c98ce8482525aa77332f54b4fff616d69
luiscavazos <luiscavazos@github.com>
2020-09-29 15:21:35 -0500

    Second commit

--- static/app.js
+++ static/app.js
@@ -1,13 +1,38 @@
-//function makePlots(id) {
+function makePlots(id) {
 
-//d3.json("static/samples.json").then(data => {
-  //  console.log(data)})
-  //  var id = data.samples.otu_id;
-  //  var values = data.samples.sample_values;
-  //  var sortvalues = values.sort((a, b) => b - a);
-  //  var finalvalues = sortvalues.slice(0,10).reverse()
-  //  var labels = data.samples.otu_labels;
-
+d3.json("static/samples.json").then(data => {
+    console.log(data)})
+    var id = data.samples.otu_id;
+    var values = data.samples.sample_values;
+    var sortvalues = values.sort((a, b) => b - a);
+    var finalvalues = sortvalues.slice(0,10).reverse()
+    var labels = data.samples.otu_labels;
+    var sortlabels = labels.sort((a, b) => b - a);
+    var finallabels = sortlabels.slice(0,10).reverse()
+    var trace = {
+        x : finalvalues,
+        y : id,
+        text: finallabels,
+        marker : {
+            color: "blue"},
+            type: "bar",
+            orientation : "h",
+        };
+        var bar = [trace];
+        var layout = {
+            title: "Top 10 OTU",
+            yaxis: {
+                tickmode:"linear",
+            },
+            margin: { 
+                l:100,
+                r:100,
+                t:100,
+                b:30
+            }
+        }
+    };
+Plotly.newPlot("bar", bar, layout);
 
 
 
