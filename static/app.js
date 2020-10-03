005b4bc63bad7b3e207e5da9f5425643a08e999f
luiscavazos <luiscavazos@github.com>
2020-10-03 16:55:09 -0500

    Test 5

--- static/app.js
+++ static/app.js
@@ -1,24 +1,81 @@
-function makePlots(id) {
+function makePlots(sample) {
 
 d3.json("static/samples.json").then(data => {
     console.log(data)})
-    var id = data.samples.otu_id;
-    var values = data.samples.sample_values;
-    var sortvalues = values.sort((a, b) => b - a);
-    var finalvalues = sortvalues.slice(0,10).reverse()
-    var labels = data.samples.otu_labels;
-    var sortlabels = labels.sort((a, b) => b - a);
-    var finallabels = sortlabels.slice(0,10).reverse()
-    var trace = {
-        x : finalvalues,
-        y : id,
-        text: finallabels,
-        marker : {
-            color: "blue"},
+
+    var samples = data.samples;
+    var resultsarray= samples.filter(x => x.id == sample);
+    var result = resultsarray[0]
+    var ids = result.otu_id;
+    var labels = result.otu_labels;
+    var values = result.sample_values;
+    
+    var LayoutBubble = {
+        margin: {t:0},
+        xaxis: {title : "Id's" },
+        hovermode: "closest"
+    };
+        var DataBubble = [{
+            x : ids,
+            y: values,
+            text : labels,
+            mode : "markers",
+            marker: {
+                color:ids,
+                size: values,
+            }
+        }];
+
+    Plotly.plot("bubble", DataBubble, LayoutBubble);   
+    
+    var bar_data = [
+        {
+            y: ids.slice(0,10).map(otuID =>`OTU ${otuID})`).reverse(),
+            x: values.slice(0,10).reverse(),
+            text: labels.slice(0,10).reverse(),
             type: "bar",
-            orientation : "h",
-        };
-        var data1 = [trace];
+            orientation:"h"
+        }
+    ]
+    var barlayout = {
+        title:"Top 10 Bacteria Cultures Found",
+        margin: { t:30, l: 150}
+    };
+    Plotly.newPlot("bar", bar_data, barlayout);
+
+function buildMetadata(sample) {
+    d3.json("static/samples.json").then(data => {
+        console.log(data);
+        var metadata= data.metadata;
+        var resultsarray = metadata.filter(d => d.id == sample);
+        var result = resultarray[0]
+        var Panel = d3.select("#sample-metadata");
+        Panel.html("");
+        Object.defineProperties(result).forEach(([key,value])=> {
+            Panel.append("h6").text(`${key}: ${value}`);
+        });
+
+    
+    })
+}
+}
+    //var id = data.samples.otu_id;
+    //var values = data.samples.sample_values;
+   // var sortvalues = values.sort((a, b) => b - a);
+    //var finalvalues = sortvalues.slice(0,10).reverse()
+    //var labels = data.samples.otu_labels;
+    //var sortlabels = labels.sort((a, b) => b - a);
+   // var finallabels = sortlabels.slice(0,10).reverse()
+   // var trace = {
+     //   x : finalvalues,
+     //   y : id,
+     //   text: finallabels,
+     //   marker : {
+     //       color: "blue"},
+     //       type: "bar",
+     //       orientation : "h",
+     //   };
+     //   var data1 = [trace];
         //var layout = {
           //  title: "Top 10 OTU",
           //  yaxis: {
@@ -30,16 +87,7 @@ d3.json("static/samples.json").then(data => {
           //      t:100,
           //      b:30
           //  }
-        }
-    Plotly.newPlot("bar", data1);
-
-
-
-function optionChanged(id) {
-    makePlots(id)
-}
-
-
+        //}
 
 
 
@@ -53,6 +101,11 @@ function init() {
         });
         makePlots(data.names[0])
     })
+}
+
+function optionChanged(newSample) {
+    makePlots(newSample);
+    buildMetadata(newSample);
 }
 
 init();
