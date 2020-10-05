157679a3a5c044b908ae9a4f3c18a8fcc50f0c76
luiscavazos <luiscavazos@github.com>
2020-10-05 16:06:41 -0500

    Final

--- static/app.js
+++ static/app.js
@@ -1,18 +1,18 @@
-function makePlots(sample) {
+function Plots(sample) {
 
 d3.json("static/samples.json").then(data => {
-    var resultsarray= data.samples.filter(x => x.id == sample);
-    var result = resultsarray[0]
+    var array= data.samples.filter(x => x.id == sample);
+    var result = array[0]
     var ids = result.otu_id;
     var labels = result.otu_labels;
     var values = result.sample_values;
     
-    var LayoutBubble = {
+    var Bubblelayout = {
         margin: {t:0},
         xaxis: {title : "Id's" },
         hovermode: "closest"
     };
-        var DataBubble = [{
+        var Bubbledata = [{
             x : ids,
             y: values,
             text : labels,
@@ -23,9 +23,9 @@ d3.json("static/samples.json").then(data => {
             }
         }];
 
-    Plotly.plot("bubble", DataBubble, LayoutBubble);   
+    Plotly.plot("bubble", Bubbledata, Bubblelayout);   
     
-    var bar_data = [
+    var bardata = [
         {
             y: ids.slice(0,10).map(otuID =>`OTU ${otuID})`).reverse(),
             x: values.slice(0,10).reverse(),
@@ -39,14 +39,14 @@ d3.json("static/samples.json").then(data => {
         margin: { t:30, l: 150}
     };
     Plotly.newPlot("bar", bar_data, barlayout)
-});
+})};
 
-function buildMetadata(sample) {
+function Metadata(sample) {
     d3.json("static/samples.json").then(data => {
         console.log(data);
         var metadata= data.metadata;
-        var resultsarray = metadata.filter(d => d.id == sample);
-        var result = resultarray[0]
+        var array = metadata.filter(d => d.id == sample);
+        var result = array[0]
         var Panel = d3.select("#sample-metadata");
         Panel.html("");
         Object.defineProperties(result).forEach(([key,value])=> {
@@ -56,37 +56,6 @@ function buildMetadata(sample) {
     
     })
 }
-}
-    //var id = data.samples.otu_id;
-    //var values = data.samples.sample_values;
-   // var sortvalues = values.sort((a, b) => b - a);
-    //var finalvalues = sortvalues.slice(0,10).reverse()
-    //var labels = data.samples.otu_labels;
-    //var sortlabels = labels.sort((a, b) => b - a);
-   // var finallabels = sortlabels.slice(0,10).reverse()
-   // var trace = {
-     //   x : finalvalues,
-     //   y : id,
-     //   text: finallabels,
-     //   marker : {
-     //       color: "blue"},
-     //       type: "bar",
-     //       orientation : "h",
-     //   };
-     //   var data1 = [trace];
-        //var layout = {
-          //  title: "Top 10 OTU",
-          //  yaxis: {
-          //      tickmode:"linear",
-          //  },
-          //  margin: { 
-          //      l:100,
-          //      r:100,
-          //      t:100,
-          //      b:30
-          //  }
-        //}
-
 
 
 function init() {
@@ -97,14 +66,14 @@ function init() {
             drop.append("option").text(sample).property("value", sample);
             
         });
-        makePlots(sampleNames[0]);
-        buildMetadata(sampleNames[0]);
+        Plots(sampleNames[0]);
+        Metadata(sampleNames[0]);
     })
 }
 
 function optionChanged(newSample) {
-    makePlots(newSample);
-    buildMetadata(newSample);
+    Plots(newSample);
+    Metadata(newSample);
 }
 
 init();
